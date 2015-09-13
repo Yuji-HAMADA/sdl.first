0217bf68325f4351a1063dc974b9064e8e1da666
Yuji HAMADA <yuji.tky.ykhm@gmail.com>
2015-09-14 00:28:32 +0900

    Make window

--- sdltest.c
+++ sdltest.c
@@ -1,5 +1,26 @@
 #include <stdlib.h>
+#include <SDL/SDL.h>
 
 int main(int argc, char *argv[]) {
+
+  SDL_Surface *gScreenSurface;
+  SDL_Event ev;
+
+  if (SDL_Init(SDL_INIT_VIDEO)) {
+    return 1;
+  }
+
+  SDL_WM_SetCaption("HELLO SDL !!", NULL);
+
+  gScreenSurface = SDL_SetVideoMode(300, 200, 24, SDL_SWSURFACE);
+
+  while(1) {
+    SDL_PollEvent(&ev);
+    if(ev.type == SDL_QUIT)
+      break;
+  }
+
+  SDL_Quit();
+  
   return 0;
 }
